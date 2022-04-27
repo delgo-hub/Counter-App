d003aec23dacd57185abd054a86842bd92ed168e
delgo123 <75376053+delgo123@users.noreply.github.com>
2022-04-27 03:48:15 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+//DOM variables
+const countEl = document.querySelector(".count");
+const btnsEl = document.querySelectorAll(".btn");
+
+//variables
+let count = 0;
+
+//click events
+for (let i = 0; i < btnsEl.length; i++) {
+    btnsEl[i].addEventListener("click", () => {
+        if (btnsEl[i].textContent === "Decrease" && count !== 0) {
+            count--;
+            countEl.textContent = count;
+        } else if (btnsEl[i].textContent === "Increase") {
+            count++;
+            countEl.textContent = count;
+        } else {
+            count = 0;
+            countEl.textContent = count;
+        }
+    });
+}
